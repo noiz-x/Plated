cb825d6b73ad8aa2447f4af7b7d35cb5051b2603
Samuel <116428301+Sam-3l@users.noreply.github.com>
2025-02-05 16:34:11 +0100

    password reset page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -7,9 +7,9 @@ import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="App flex flex-col h-screen">
         <Navbar />
-        <div className="content bg-slate-200">
+        <div className="content bg-slate-200 flex-grow">
           <AppRoutes />
         </div>
       </div>
--- Frontend/src/pages/ResetPassword.jsx
+++ Frontend/src/pages/ResetPassword.jsx
@@ -1,5 +1,5 @@
 import { useParams, Link, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const ResetPassword = () => {
     const { uidb64, key } = useParams();
@@ -7,8 +7,13 @@ const ResetPassword = () => {
     const navigate = useNavigate();
 
     const [redirectTimer, setRedirectTimer] = useState(10);
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
+    const [level, setLevel] = useState(1);
+
+    // form
+    const [password1, setPassword1] = useState("");
+    const [password2, setPassword2] = useState("");
 
     const handleRedirect = () => {
         let time = 10
@@ -23,19 +28,23 @@ const ResetPassword = () => {
         }, 1000)
     }
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        setIsLoading(true);
+        e.preventDefault();
+
         fetch(`${apiDomain}api/auth/password/reset/confirm/`, {
             headers: {"Content-Type": "application/json"},
             method: "POST",
-            body: JSON.stringify({ uid: uidb64, token: key })
+            body: JSON.stringify({ new_password1: password1, new_password2: password2, uid: uidb64, token: key })
         })
         .then(res => {
             if (res.ok){
                 setIsLoading(false);
-                handleRedirect();
+                setError(null);
+                setLevel(2);
             }
             else{
-                throw Error("We could'nt change your password. Link may be invalid or expired.");
+                throw Error("Password Reset Failed. Link may be invalid or expired.");
             }
         })
         .catch(err => {
@@ -45,8 +54,69 @@ const ResetPassword = () => {
     }
 
     return ( 
-        <div className="password-reset">
-            I'll finish this password reset page tomorrow.
+        <div className="reset-password py-10 sm:px-10 px-3">
+            <div className="bg-white xl:mx-96 lg:mx-64 md:mx-40 sm:mx-10 rounded-lg shadow-lg p-6 sm:p-10">
+                {level === 1 && (
+                    <div>
+                        <div className="text-2xl text-center mb-3 font-semibold">
+                            Reset Your Password
+                        </div>
+
+                        {error && (
+                            <div className="error">
+                                { error }
+                            </div>
+                        )}
+
+                        <form onSubmit={e => handleSubmit(e)}>
+                            <div>
+                                <label htmlFor="password1">Enter New Password</label>
+                                <input 
+                                    type="password"
+                                    id="password1"
+                                    className="input"
+                                    required
+                                    placeholder="Input your new password"
+                                    minLength={8}
+                                    value={password1}
+                                    onChange={(e) => setPassword1(e.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password2">Confirm Your Password</label>
+                                <input 
+                                    type="password"
+                                    id="password2" 
+                                    className="input"
+                                    placeholder="Confirm your password"
+                                    required
+                                    value={password2}
+                                    minLength={8}
+                                    onChange={(e) => setPassword2(e.target.value)}
+                                />
+                            </div>
+                            <div className="mt-5">
+                                <button type="submit" className="btn w-full" disabled={isLoading} style={{
+                                    opacity: isLoading && 0.6,
+                                }}>{ isLoading && "Setting Password..." }{ !isLoading && "Set Password"}</button>
+                            </div>
+
+                        </form>
+                    </div>
+                )}
+
+                { level === 2 && (
+                    <div className="email-sent p-3">
+                        <div>
+                            Successful password reset
+
+                            Login with new passowrd now?
+                            Redirects to login automatically.
+                        </div>
+                    </div>
+                )}
+
+            </div>
         </div>
      );
 }
