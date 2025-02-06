c079ebad5cd62d8c4b277ca840926cb2f0e4cef5
Samuel <116428301+Sam-3l@users.noreply.github.com>
2025-02-06 22:30:17 +0100

    password reset success page

--- Frontend/src/pages/ResetPassword.jsx
+++ Frontend/src/pages/ResetPassword.jsx
@@ -9,7 +9,7 @@ const ResetPassword = () => {
     const [redirectTimer, setRedirectTimer] = useState(10);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
-    const [level, setLevel] = useState(1);
+    const [level, setLevel] = useState(2);
 
     // form
     const [password1, setPassword1] = useState("");
@@ -111,15 +111,16 @@ const ResetPassword = () => {
                 )}
 
                 { level === 2 && (
-                    <div className="email-sent p-3">
-                        <div>
-                            Successful password reset
-
-                            <div>Login with new passowrd now?</div>
-                            Redirects to login automatically.
-
-                            Nice works
+                    <div className="flex-col justify-center p-4">
+                        <svg width="40" height="40" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="mb-4 mx-auto">
+                            <path fillRule="evenodd" d="M10 16L12 14V10L13.6569 8.34314C15.1571 6.84285 16 4.80802 16 2.68629V0H13.3137C11.192 0 9.15714 0.842855 7.65685 2.34315L6 4H2L0 6L10 16ZM10.5 7C11.3284 7 12 6.32843 12 5.5C12 4.67157 11.3284 4 10.5 4C9.67157 4 9 4.67157 9 5.5C9 6.32843 9.67157 7 10.5 7Z" fill="#00b"/>
+                            <path d="M4.9274 13.7558L2.24423 11.0726L0 15L1 16L4.9274 13.7558Z" fill="#00b"/>
+                        </svg>
+                        <div className="text-center">Your email has been verified successfully.</div>
+                        <div className="my-3">
+                            <Link to="/login"><button className="btn w-full">Log in now</button></Link>
                         </div>
+                        <div className="text-center">Redirects to login automatically in {redirectTimer}s</div>
                     </div>
                 )}
 
