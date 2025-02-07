033d29687c00f7e504e4c928530a0c7183345674
Samuel <116428301+Sam-3l@users.noreply.github.com>
2025-02-07 15:08:07 +0100

    updated App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import 'flowbite';
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 import AppRoutes from "./routes/AppRoutes";
 import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
+  const isLoggedIn = !!localStorage.getItem("authToken"); 
+
   return (
     <Router>
       <div className="App flex flex-col h-screen">
-        <Navbar />
+        {isLoggedIn ? <Sidebar /> : <Navbar />}
         <div className="content bg-slate-200 flex-grow">
           <AppRoutes />
         </div>
