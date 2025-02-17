3af29d47a631a5ece09ddbba61c9ffcbe7edc06f
Samuel <116428301+Sam-3l@users.noreply.github.com>
2025-02-17 13:13:35 +0100

    finished speed dial design

--- Frontend/src/components/SpeedDial.jsx
+++ Frontend/src/components/SpeedDial.jsx
@@ -12,6 +12,7 @@ import {
     CogIcon,
     Square3Stack3DIcon,
   } from "@heroicons/react/24/outline";
+import { CalendarDaysIcon, DocumentPlusIcon, ShoppingCartIcon } from "@heroicons/react/24/solid";
    
 function SpeedDialComp() {
     const labelProps = {
@@ -25,22 +26,22 @@ function SpeedDialComp() {
         <div className="absolute bottom-5 right-5">
           <SpeedDial>
             <SpeedDialHandler>
-              <IconButton size="lg" className="rounded-full">
-                <PlusIcon className="h-5 w-5 transition-transform group-hover:rotate-45" />
+              <IconButton size="lg" className="rounded-full bg-blue-800">
+                <PlusIcon className="h-5 w-5 transition-transform group-hover:rotate-45 text-white" />
               </IconButton>
             </SpeedDialHandler>
             <SpeedDialContent>
-              <SpeedDialAction className="relative">
-                <HomeIcon className="h-5 w-5" />
-                <Typography {...labelProps}>Home</Typography>
+              <SpeedDialAction className="relative bg-blue-800 hover:bg-blue-900">
+                <DocumentPlusIcon className="h-5 w-5 text-white" />
+                <Typography {...labelProps}>Recipe</Typography>
               </SpeedDialAction>
-              <SpeedDialAction className="relative">
-                <CogIcon className="h-5 w-5" />
-                <Typography {...labelProps}>Settings</Typography>
+              <SpeedDialAction className="relative bg-blue-800 hover:bg-blue-900">
+                <CalendarDaysIcon className="h-5 w-5 text-white" />
+                <Typography {...labelProps}>Plan</Typography>
               </SpeedDialAction>
-              <SpeedDialAction className="relative">
-                <Square3Stack3DIcon className="h-5 w-5" />
-                <Typography {...labelProps}>Pages</Typography>
+              <SpeedDialAction className="relative bg-blue-800 hover:bg-blue-900">
+                <ShoppingCartIcon className="h-5 w-5 text-white" />
+                <Typography {...labelProps}>Grocery</Typography>
               </SpeedDialAction>
             </SpeedDialContent>
           </SpeedDial>
