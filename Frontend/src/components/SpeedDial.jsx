91678ab91d787f4ece92eed2d5b852366eb7a642
Samuel <116428301+Sam-3l@users.noreply.github.com>
2025-02-19 15:03:10 +0100

    added link to speed dial button

--- Frontend/src/components/SpeedDial.jsx
+++ Frontend/src/components/SpeedDial.jsx
@@ -8,11 +8,9 @@ import {
   } from "@material-tailwind/react";
   import {
     PlusIcon,
-    HomeIcon,
-    CogIcon,
-    Square3Stack3DIcon,
   } from "@heroicons/react/24/outline";
 import { CalendarDaysIcon, DocumentPlusIcon, ShoppingCartIcon } from "@heroicons/react/24/solid";
+import { useNavigate } from "react-router-dom";
 
 // still a lil problem on z-index of speed dial
 // also way to darken background when speed dial is hovered
@@ -24,6 +22,8 @@ function SpeedDialComp() {
       className:
         "absolute top-2/4 -left-2/4 -translate-y-2/4 -translate-x-3/4 font-normal",
     };
+
+    const navigate = useNavigate();
    
     return (
         <div className="fixed bottom-5 right-5 z-40">
@@ -34,7 +34,7 @@ function SpeedDialComp() {
               </IconButton>
             </SpeedDialHandler>
             <SpeedDialContent>
-              <SpeedDialAction className="relative bg-blue-800 hover:bg-blue-900">
+              <SpeedDialAction className="relative bg-blue-800 hover:bg-blue-900" onClick={() => navigate("/recipes/new")}>
                 <DocumentPlusIcon className="h-5 w-5 text-white" />
                 <Typography {...labelProps}>Recipe</Typography>
               </SpeedDialAction>
