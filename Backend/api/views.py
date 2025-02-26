2669e8c1788d826ad3b7696d724900a057a73e1a
Samuel <116428301+Sam-3l@users.noreply.github.com>
2025-02-26 12:48:33 +0100

    also added a view to access all recipes saved by a user

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -14,7 +14,7 @@ class RecipeViewset(viewsets.ModelViewSet):
     serializer_class = RecipeSerializer
 
     def get_queryset(self):
-        if self.action in ['public_list', 'public_details', 'save']:  # All recipes for public actions
+        if self.action in ['public_list', 'public_details', 'save', 'saves',]:  # All recipes for public actions
             return Recipe.objects.select_related('author').all()
         return Recipe.objects.filter(author=self.request.user).select_related('author').all()
 
@@ -24,7 +24,7 @@ class RecipeViewset(viewsets.ModelViewSet):
     @action(detail=False, methods=['get',], permission_classes=[AllowAny,], url_path='public-list')
     def public_list(self, request):
         """
-        Public Recipe Endpoint - with read-only access to all recipes. 
+        Public Recipe View - with read-only access to all recipes. 
         Accessible to all users (even unauthenticated users)
         """
         recipes = self.get_queryset()
@@ -34,7 +34,7 @@ class RecipeViewset(viewsets.ModelViewSet):
     @action(detail=True, methods=['get',], permission_classes=[AllowAny,], url_path='public-details')
     def public_details(self, request, pk):
         """
-        Public Recipe Endpoint - with read-only access to specific recipe.
+        Public Recipe View - with read-only access to specific recipe.
         Accessible to all users (even unauthenticated users)
         """
         recipe = self.get_object()
@@ -44,7 +44,7 @@ class RecipeViewset(viewsets.ModelViewSet):
     @action(detail=True, methods=['patch',], permission_classes=[IsAuthenticated,])
     def save(self, request, *args, **kwargs):
         """
-        Endpoint to save or un-save recipes.
+        View to save or un-save recipes.
         Accessible to only authenticated users.
         Saves are private.
         """
@@ -56,6 +56,16 @@ class RecipeViewset(viewsets.ModelViewSet):
         else:
             recipe.saved_by.add(user)
             return Response({'detail': 'Recipe saved'}, status=status.HTTP_201_CREATED)
+        
+    @action(detail=False, methods=['get',], permission_classes=[IsAuthenticated,])
+    def saves(self, request):
+        """
+        View to get all recipes saved by a user.
+        Clearly, accessible to only authenticated users
+        """
+        recipes = self.get_queryset().filter(saved_by=request.user)
+        serializer = PublicRecipeSerializer(recipes, many=True)
+        return Response(serializer.data)
 
 class PublicUserViewSet(viewsets.GenericViewSet):
     """Public user viewset with limited public actions: profile view and follow."""
