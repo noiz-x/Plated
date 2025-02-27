b46ec059fa943c7ce70d3181b7d184ccc0ea1de0
Samuel <116428301+Sam-3l@users.noreply.github.com>
2025-02-27 13:37:40 +0100

    sorting out default rating val

--- Backend/api/signals.py
+++ Backend/api/signals.py
@@ -18,6 +18,6 @@ def update_rating(sender, instance, **kwargs):
     Automatically update the rating (overall rating) of a recipe whenever a rating is added, updated, or deleted.
     """
     recipe = instance.recipe
-    average_rating = RecipeRatingsAndReviews.objects.filter(recipe=instance).aggregate(avg_rating=Avg('rating'))['avg_rating'] or None
+    average_rating = RecipeRatingsAndReviews.objects.filter(recipe=instance).aggregate(avg_rating=Avg('rating'))['avg_rating'] or 0
     recipe.rating = average_rating
     recipe.save()
