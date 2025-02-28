bc923c20a1e3be53f9b6c1fbf3143e421402a169
Samuel <116428301+Sam-3l@users.noreply.github.com>
2025-02-28 16:48:45 +0100

    removed resolved indication

--- Backend/database/models.py
+++ Backend/database/models.py
@@ -60,7 +60,7 @@ class RecipeRatingsAndReviews(models.Model):
     """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    rating = models.DecimalField(max_digits=2, decimal_places=1, default=0, help_text="Rating from 0.0 to 5.0") # Work to be done on default value
+    rating = models.DecimalField(max_digits=2, decimal_places=1, default=0, help_text="Rating from 0.0 to 5.0")
     comment = models.TextField(blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
