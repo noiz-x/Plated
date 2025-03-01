228f0221f8bb855876c5ce9b7edcb31793d7bee2
Samuel <116428301+Sam-3l@users.noreply.github.com>
2025-03-01 18:34:43 +0100

    url and routing

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 
 router.register(r'recipes', views.RecipeViewset, basename='recipe')
-router.register(r'rate-and-review', views.RatingsAndReviewsViewset, basename='rate-and-review')
+router.register(r'ratings-and-reviews', views.RatingsAndReviewsViewset, basename='rate-and-review')
 
 urlpatterns = [
     path('', include(router.urls)),
