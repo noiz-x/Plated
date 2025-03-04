48db1035011161a331f14299c5d4d201fd615257
Samuel <116428301+Sam-3l@users.noreply.github.com>
2025-03-04 18:53:04 +0100

    signals

--- Backend/api/signals.py
+++ Backend/api/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import m2m_changed, post_save, post_delete
 from django.dispatch import receiver
 from django.db.models import Avg
-from database.models import Recipe, RecipeRatingsAndReviews
+from database.models import Recipe, RecipeRatingsAndReviews, User
 
 @receiver(m2m_changed, sender=Recipe.saved_by.through)
 def update_saves(sender, instance, action, **kwargs):
@@ -22,3 +22,8 @@ def update_rating(sender, instance, **kwargs):
     average_rating = ratings.aggregate(avg_rating=Avg('rating'))['avg_rating'] or 0
     recipe.rating = average_rating
     recipe.save()
+
+@receiver(m2m_changed, sender=User.followers.through)
+def update_followers(sender, instance, action, **kwargs):
+    pass
+    # i'll be back to fix this up soon
